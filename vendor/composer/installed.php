5f6ef5389f8db510312be43f0b3a3d73c18e6a85
Andy Fragen <andy@thefragens.com>
2024-11-28 13:04:19 -0800

    add check for auth header

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b0db3ea4680ef8cfbb881d192e581bfe5b928c6b',
+        'reference' => 'ad8b97938f0f7df3aa7bf17200e8f1450170001f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b0db3ea4680ef8cfbb881d192e581bfe5b928c6b',
+            'reference' => 'ad8b97938f0f7df3aa7bf17200e8f1450170001f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'afragen/git-updater-lite' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '50ebe8cc2712338b173544dc48dd7aea283ca5f3',
+            'reference' => '76f8c459b6877b34ad0fc744f8b7498bda4a9407',
             'type' => 'library',
             'install_path' => __DIR__ . '/../afragen/git-updater-lite',
             'aliases' => array(
