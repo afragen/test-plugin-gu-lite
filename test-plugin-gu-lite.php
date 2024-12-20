ad6bfa0ee4804727b5a40db5e1da708cd3536b54
Andy Fragen <andy@thefragens.com>
2024-12-20 14:45:20 -0800

    update for GUL 1.3.0

--- test-plugin-gu-lite.php
+++ test-plugin-gu-lite.php
@@ -3,7 +3,7 @@
  * Plugin Name: Test Plugin Git Updater Lite
  * Plugin URI: https://github.com/afragen/test-plugin-gu-lite/
  * Description: This plugin is used for testing functionality of Github updating of plugins.
- * Version: 0.1.2
+ * Version: 0.1.3
  * Author: Andy Fragen
  * License: MIT
  * GitHub Plugin URI: https://github.com/afragen/test-plugin-gu-lite/
@@ -19,6 +19,8 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+add_filter( 'gul_update_server', function () {
+    return 'https://thefragens.net';
+});
 require_once __DIR__ . '/vendor/afragen/git-updater-lite/Lite.php';
-( new \Fragen\Git_Updater\Lite( __FILE__ ) )
-    ->run( 'https://thefragens.net/wp-json/git-updater/v1/plugins-api/?slug=test-plugin-gu-lite' );
+( new \Fragen\Git_Updater\Lite( __FILE__ ) )->run();
