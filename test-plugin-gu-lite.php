1d9c8dc31333695e0b71a621178f8ceb45470baf
Andy Fragen <andy@thefragens.com>
2024-12-28 14:57:55 -0800

    Add Update URI and bump

--- test-plugin-gu-lite.php
+++ test-plugin-gu-lite.php
@@ -3,11 +3,12 @@
  * Plugin Name: Test Plugin Git Updater Lite
  * Plugin URI: https://github.com/afragen/test-plugin-gu-lite/
  * Description: This plugin is used for testing functionality of Github updating of plugins.
- * Version: 0.1.4
+ * Version: 0.1.5
  * Author: Andy Fragen
  * License: MIT
  * Requires WP: 6.6
  * Requires PHP: 8.0
+ * Update URI: afragen/test-plugin-gu-lite
  */
 
  /**
