ade0f821ac77920857d50f33f7973865844a3ba4
Andy Fragen <andy@thefragens.com>
2025-02-11 15:56:32 -0800

    use Update URI header instead of filter with git-updater-lite@2.0.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,5 @@
+#### 0.2.0
+* update to `afragen/git-updater-lite@2.0.0`
 
 #### 1.0
 * This is not a real version number and is just for testing purposes.
--- test-plugin-gu-lite.php
+++ test-plugin-gu-lite.php
@@ -3,12 +3,12 @@
  * Plugin Name: Test Plugin Git Updater Lite
  * Plugin URI: https://github.com/afragen/test-plugin-gu-lite/
  * Description: This plugin is used for testing functionality of Github updating of plugins.
- * Version: 0.1.10
+ * Version: 0.2.0
  * Author: Andy Fragen
  * License: MIT
  * Requires WP: 6.6
  * Requires PHP: 8.0
- * Update URI: afragen/test-plugin-gu-lite
+ * Update URI: https://git-updater.com
  */
 
  /**
@@ -19,7 +19,4 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 require_once __DIR__ . '/vendor/afragen/git-updater-lite/Lite.php';
-add_filter( 'gul_update_server', function () {
-    return 'https://git-updater.com';
-});
 ( new \Fragen\Git_Updater\Lite( __FILE__ ) )->run();
