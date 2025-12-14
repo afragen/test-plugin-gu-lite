33e55dd5c65bba65e17e611535dd5966148a4240
Andy Fragen <andy@thefragens.com>
2025-12-14 14:43:48 -0800

    composer update

--- vendor/afragen/git-updater-lite/Lite.php
+++ vendor/afragen/git-updater-lite/Lite.php
@@ -79,11 +79,10 @@ if ( ! class_exists( 'Fragen\\Git_Updater\\Lite' ) ) {
 		 * @return string|WP_Error
 		 */
 		private function check_update_uri( $updateUri ) {
-			if ( filter_var( $updateUri, FILTER_VALIDATE_URL )
-				&& null === parse_url( $updateUri, PHP_URL_PATH ) // null means no path is present.
+			$updateUri = untrailingslashit( trim( $updateUri ) );
+			if ( ! ( filter_var( $updateUri, FILTER_VALIDATE_URL )
+				&& null === parse_url( $updateUri, PHP_URL_PATH ) ) // null means no path is present.
 			) {
-				$updateUri = untrailingslashit( trim( $updateUri ) );
-			} else {
 				return new WP_Error( 'invalid_header_data', 'Invalid data from Update URI header', $updateUri );
 			}
 
@@ -136,15 +135,9 @@ if ( ! class_exists( 'Fragen\\Git_Updater\\Lite' ) ) {
 				}
 				$this->api_data->file = $this->file;
 
-				/*
-				* Set transient for 5 minutes as AWS sets 5 minute timeout
-				* for release asset redirect.
-				*
-				* Set limited timeout so wp_remote_get() not hit as frequently.
-				* wp_remote_get() for plugin/theme check can run on every pageload
-				* for certain pages.
-				*/
-				set_site_transient( "git-updater-lite_{$this->file}", $this->api_data, 5 * MINUTE_IN_SECONDS );
+				// Set timeout for transient via filter.
+				$timeout = apply_filters( 'git_updater_lite_transient_timeout', 6 * HOUR_IN_SECONDS, $this->api_data );
+				set_site_transient( "git-updater-lite_{$this->file}", $this->api_data, $timeout );
 			} else {
 				if ( property_exists( $response, 'error' ) ) {
 					return new WP_Error( 'repo-no-exist', 'Specified repo does not exist' );
